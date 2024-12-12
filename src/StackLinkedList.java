817b0d0cf6438e5ec549ccde67c2c32ebe0aca57
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-12 11:42:45 +0530

    stack operation

--- src/StackLinkedList.java
+++ src/StackLinkedList.java
@@ -10,15 +10,40 @@ public class StackLinkedList {
     //push
     public void push(int data){
         Node node=new Node(data);
+        node.next=top;
+        top=node;
 
 
     }
 
 
     //pop
+    public int  pop(){
+        if(isEmpty()){
+            System.out.println("List is empty");
+            return -1;
+        }
+        else {
+
+            int value=top.data;
+            top=top.next;
+            return value;
+
+
+        }
+    }
 
 
     //peek
+    public int peek(){
+        if(isEmpty()){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        else {
+            return top.data;
+        }
+    }
 
 
     //isEmpty
@@ -34,5 +59,21 @@ public class StackLinkedList {
 
 
     //display
+
+
+    public void  display(){
+        if(isEmpty()){
+            System.out.println("List is Empty");
+            return;
+        }
+        else{
+            Node temp=top;
+            while(temp!=null){
+                System.out.println(temp.data+"\t");
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+    }
 
 }
