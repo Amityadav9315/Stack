7d24ee803248a3720e44fbab2742622544d96f61
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-12-12 11:23:52 +0530

    Stack operation

--- src/Stack.java
+++ src/Stack.java
@@ -3,4 +3,32 @@ public class Stack {
     private int[] array;
     private int maxSize;
     private int top=-1;
+
+    public Stack(int size){
+        this.maxSize=size;
+        array=new int[maxSize];
+        top=-1;
+
+    }
+    //push operation
+    public void push(int data){
+        if(top==maxSize-1){
+            System.out.println("Stack overflow");
+            return;
+        }
+        else{
+            top++;
+            array[top]=data;
+        }
+    }
+    //display data
+    public void displayElement(){
+        if(top==-1){
+            System.out.println("Stack is empty");
+        }
+        for(int i=0; i<=top; i++){
+            System.out.println(array[i]+"\t");
+        }
+        System.out.println();
+    }
 }
